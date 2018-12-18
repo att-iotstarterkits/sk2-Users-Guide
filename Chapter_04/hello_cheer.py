1c05aa8a3c1da0a9c263e6f7d1a8f4fe4b46aea8
SGSPECKER <scott@sgspecker.com>
2018-12-18 01:39:38 -0600

    Added first examples for Chapter 4

---
+++ Chapter_04/hello_cheer.py
@@ -0,0 +1,8 @@
+# hello_cheer.py
+
+msg = 'Hello World'
+
+for c in msg:
+    print("Give me a '" + c + "'")
+
+print("\nWhat does it spell? \n " + msg)
---
+++ Chapter_04/my_func.py
@@ -0,0 +1,7 @@
+# my_func example
+
+def my_func(a):
+	print(a)
+
+# Using the function my_func()
+my_func('Print me!')
