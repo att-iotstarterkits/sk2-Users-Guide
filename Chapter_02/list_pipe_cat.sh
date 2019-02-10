c3c26386f7be5552dd1357c604351c89c6e8247b
SGSPECKER <scott@sgspecker.com>
2019-02-10 16:25:14 -0600

    Tweaked comments

--- Chapter_02/list_pipe_cat.sh
+++ Chapter_02/list_pipe_cat.sh
@@ -1,11 +1,12 @@
 # list_pipe_cat.sh
 #
-# - This script writes a listing of /dev directory
-#   into the file mylisting.txt in /CUSTAPP
-# - The ">" pipes tells the shell to pipe the output
-#   into the txt file rather than to the standard output
+# - This script writes a listing of /dev directory into
+#   a file named "mylisting.txt" in /CUSTAPP
+# - The ">" pipe symbole tells the shell to pipe the output
+#   from the "ls -ls /dev" command into the txt file rather 
+#   than printing it to the standard output
 # - It then prints out the contents of mylisting.txt
-#   using the "cat" command
+#   using the Linux "cat" command
 #
 ls -ls /dev > /CUSTAPP/mylisting.txt
 cat /CUSTAPP/mylisting.txt
